351bfbf1f4d59eeec8a57b1b9166b953feab44c4
Michael E. Palmer <mep@mepalmer.net>
2016-05-08 13:42:09 -0700

    turned off csrf checking! fixed scoring with post, and "back" button. also added 'restrict-if-score-above'

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -1,52 +1,46 @@
 class NodesController < ApplicationController
-  before_action :update_score
+  before_action :get_params
 
   def index
-
   end
 
   def show
-    name = ActionController::Base.helpers.sanitize(params[:name])
-    #puts "name is #{name}"
-    render "#{name}.html.md"
+    render "#{@name}.html.md"
   end
 
   private
 
-  def set_score
-    @score = session[:score] || 0
-    @chain = session[:chain] || []
-  end
-
-  def update_score
-    set_score
-
+  def get_params
     hash = {
-        "max": -1,
-        "melody": +1,
+        "barbie_1" => -1,
+        "barbie_3" => +1,
 
-        "weak": -1,
-        "strong": +1,
+        "second_grade_1" => -1,
+        "second_grade_3" => +1,
 
-        "laugh": -1,
-        "explain": +1
-
+        "teams_1" => -1,
+        "teams_3" => +1,
     }.with_indifferent_access
 
+    @score = 0
     if controller_name == "nodes" and action_name == "index"
-      @score = 0
       @chain = []
     elsif controller_name == "nodes" and action_name == "show"
-      name = params[:name]
+      @name = ActionController::Base.helpers.sanitize(params[:name])
+      chain =  ActionController::Base.helpers.sanitize(params[:chain])
+      @chain = chain.blank? ? [] : JSON.parse(chain)
+      # puts "@name is #{@name}"
+      # puts "old @chain is #{@chain}"
+      # puts "old @score is #{@score}"
+      # @chain << @name
 
-      if !@chain.include?(name)
-        @chain << name
-        delta = hash[name.parameterize.underscore.to_sym]
+      @chain.each do |n|
+        delta = hash[n.parameterize.underscore.to_sym]
         @score += delta if delta
       end
+      # puts "new @chain is #{@chain}"
+      # puts "new @score is #{@score}"
     end
-    session[:score] = @score
-    session[:chain] = @chain
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'nodes#index'
   get 'node/:name' => 'nodes#show'
+  post 'node/:name' => 'nodes#show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
